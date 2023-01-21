ae9db2b040222a261a78fd79ca1b27c9ed5a4cc0
Мария <marklukanova@gmail.com>
2023-01-22 01:39:52 +0300

    Updates

---
+++ js/map.js
@@ -0,0 +1,1 @@
+let mapImage=document.querySelector(".contacts__map-link"),map=document.querySelector(".contacts__map");mapImage.classList.add("contacts__map-link--hidden"),map.classList.remove("contacts__map--nojs");
--- source/js/map.js
+++
@@ -1,5 +0,0 @@
-let mapImage = document.querySelector('.contacts__map-link');
-let map = document.querySelector('.contacts__map');
-
-mapImage.classList.add('contacts__map-link--hidden');
-map.classList.remove('contacts__map--nojs');
